8ee11e42ff9fee006075ed679721d93f0c025882
Saraansh <gandhisaraansh@gmail.com>
2026-05-05 03:37:28 +0530

    Add FCFS scheduling algorithm implementation
    Implement First-Come, First-Served (FCFS) scheduling algorithm to calculate completion time, turnaround time, and waiting time for processes.

---
+++ fcfs.py
@@ -0,0 +1,31 @@
+p = ["P1","P2","P3","P4"]
+at = [0,1,2,3]
+bt = [5,3,8,6]
+
+n = len(p)
+
+ct = [0]*n
+tat = [0]*n
+wt = [0]*n
+
+ct[0] = at[0] + bt[0]
+for i in range(1,n):
+    if ct[i-1] < at[i]:
+        ct[i] = at[i] + bt[i]
+    else:
+        ct[i] = ct[i-1] + bt[i]
+
+for i in range(n):
+    tat[i] = ct[i] - at[i]
+    wt[i] = tat[i] - bt[i]
+
+avg_tat = sum(tat)/n
+avg_wt = sum(wt)/n
+
+print("P  AT  BT  CT  TAT  WT")
+for i in range(n):
+    print(p[i], at[i], bt[i], ct[i], tat[i], wt[i])
+
+print("\n")
+print("Average TAT =", avg_tat)
+print("Average WT =", avg_wt)
