a21242287ca58e6e43142ac766abee249fff803d
Saraansh <gandhisaraansh@gmail.com>
2026-05-05 04:57:18 +0530

    Update rr.py

--- rr.py
+++ rr.py
@@ -1,48 +1,47 @@
 p = ["P1","P2","P3","P4","P5"]
-at = [0,2,4,6,8]
-bt = [5,3,1,2,4]
+at =[ 0 ,5,7,1,2]
+bt = [5,6,7,8,9]
 
 n = 5
-tq = 3
+tq=3
 
 rem_bt = bt.copy()
-ct = [0]*n
-tat = [0]*n
-wt = [0]*n
+tat=[0]*n
+wt=[0]*n
+ct=[0]*n
 
-time = 0
-queue = []
-visited = [0]*n
+time = 0 
+visited=[0]*n
+queue=[]
 
-# first process
 queue.append(0)
 visited[0] = 1
 
 while queue:
     i = queue.pop(0)
-
-    if rem_bt[i] > tq:
-        time += tq
-        rem_bt[i] -= tq
-    else:
-        time += rem_bt[i]
+    
+    if rem_bt[i]>tq:
+        time+=tq
+        rem_bt[i] -=tq
+    else :
+        time+= rem_bt[i]
         rem_bt[i] = 0
         ct[i] = time
-
-    # add newly arrived processes
-    for j in range(n):
-        if at[j] <= time and visited[j] == 0:
+        
+    for j in range (n) :
+        if at[j] <= time and visited[j] ==0:
             queue.append(j)
             visited[j] = 1
-
-    # if process not finished, push again
-    if rem_bt[i] > 0:
+            
+    if rem_bt[i]> 0:
         queue.append(i)
 
-# calculate TAT & WT
 for i in range(n):
     tat[i] = ct[i] - at[i]
     wt[i] = tat[i] - bt[i]
+
+avg_tat = sum(tat)/n
+avg_wt = sum (wt)/n
 
 avg_tat = sum(tat)/n
 avg_wt = sum(wt)/n
